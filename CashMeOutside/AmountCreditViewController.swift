bc59654225ceb6b9417572aff2c5df5cc1a8633a
Biswash Adhikari <biswash.adhikari@bison.howard.edu>
2018-02-10 18:05:51 -0500

    add initail, amount, amountcredit viewcontroller

---
+++ CashMeOutside/AmountCreditViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AmountCreditViewController.swift
+//  CashMeOutside
+//
+//  Created by Biswash Adhikari on 2/10/18.
+//  Copyright © 2018 Anish Adhikari. All rights reserved.
+//
+
+import UIKit
+
+class AmountCreditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- CashMeOutside/ViewController.swift
+++ CashMeOutside/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var receiveButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
